8d649b333ebd33133ffdf7d3d8321cf803a420d4
Sohan Venkatesh <126096232+sohv@users.noreply.github.com>
2023-06-14 15:21:30 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a to-do list app created using MERN stack and deployed on Vercel.
+The database used is MongoDB
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const todoRoute = require('./routes/todos')
+const mongoose = require('mongoose')
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/todos', todoRoute)
+mongoose.connect("mongo connection string").then(data => {
+   console.log("connected to DB")
+}).catch(error => {
+   console.log(error)
+})
+app.listen(3000)
